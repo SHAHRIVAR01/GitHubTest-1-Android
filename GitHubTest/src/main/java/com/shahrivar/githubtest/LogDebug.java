e0fd7c42ad3bc35f85c16f489f11a09127e347c7
Abolfazl sarikhanikhorrami <shahrivardeveloper@gmail.com>
2021-10-25 10:23:47 -0700

    create project in studio . create LogDebug for easy debugging .

---
+++ GitHubTest/src/main/java/com/shahrivar/githubtest/LogDebug.java
@@ -0,0 +1,9 @@
+package com.shahrivar.githubtest;
+
+import android.util.Log;
+
+public class LogDebug {
+    public static void getLog(String tag,String massage){
+        Log.d(tag,massage);
+    }
+}
